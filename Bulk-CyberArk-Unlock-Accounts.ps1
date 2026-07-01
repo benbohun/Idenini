0842820f6237c0e82a463e8f92f789619e66c3f1
benbohun <131300582+benbohun@users.noreply.github.com>
2026-07-01 11:16:30 -0400

    Update Bulk-CyberArk-Unlock-Accounts.ps1

--- Bulk-CyberArk-Unlock-Accounts.ps1
+++ Bulk-CyberArk-Unlock-Accounts.ps1
@@ -1,12 +1,1 @@
-[System.Net.WebRequest]::DefaultWebProxy = New-Object System.Net.WebProxy("http://proxy.prod.mtb.com:8080") # proxy
-$cred = Get-Credential -Message "Enter api account username and password" # Your ISPSS identity or service user credentials
-New-PASSession -Credential $cred -TenantSubdomain "mtbank" -ServiceUser
-
-$fd = "account_ids.txt" #list of account IDs
-$data = (Get-Content -Raw $fd).Trim() -split "`n" 
-$data | % {
-    [array]$operations = @{"op"="replace";"path"="/secretManagement/automaticManagementEnabled";"value"=$true}
-    [array]$operations += @{"op"="replace";"path"="/secretManagement/manualmanagementreason"; value=""}
-    Set-PASAccount -ID $_ -operations $operations
-}
-Close-PASSession
+...
